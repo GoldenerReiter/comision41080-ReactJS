a71e73144b5cd4c26481eb473dbe5600ad4666dc
GoldenerReiter <rcosero@outlook.com>
2022-07-13 19:58:11 -0400

    App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './jsmodules/NavBar/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
